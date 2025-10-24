d37b68db764cf051c4b09599c9229bfcf793f166
gamer-boy-alt <onye7899@gmail.com>
2025-10-24 22:01:43 +0100

    Changed colors in the projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,43 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="sticky top-0 z-50 bg-white shadow-sm">
-      <div className="flex items-center justify-between max-w-6xl px-4 py-4 mx-auto">
-        <h1 className="text-2xl font-bold text-indigo-600">Ifeabunike Onyedika</h1>
-        <div className="space-x-6">
-          <Link to="/" className="hover:text-indigo-600">Home</Link>
-          <Link to="/projects" className="hover:text-indigo-600">Projects</Link>
-          <Link to="/about" className="hover:text-indigo-600">About</Link>
-          <Link to="/contact" className="hover:text-indigo-600">Contact</Link>
+      <div className="flex items-center justify-between h-full max-w-6xl px-4 py-4 mx-auto">
+        <h1 className="h-full text-2xl font-bold text-blue-500">My Portfolio</h1>
+        <div className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+          &#9776;
+        </div>
+        {                  }
+        <div className="hidden space-x-6 lg:flex">
+          <Link to="/" className="">Home</Link>
+          <Link to="/projects" className="">Projects</Link>
+          <Link to="/about" className="">About</Link>
+          <Link to="/contact" className="">Contact</Link>
         </div>
       </div>
+      {                 }
+      {isOpen && (
+        <div className="border-2 lg:hidden p-7">
+          <ul className="flex flex-col items-center justify-center space-y-4">
+            <li>
+              <Link to="/" className="">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects" className="">Projects</Link>
+            </li>
+            <li>
+              <Link to="/about" className="">About</Link>
+            </li>
+            <li>
+              <Link to="/contact" className="">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center px-4 py-24 text-center">
-      <h1 className="mb-4 text-4xl font-bold">Hi, I'm Ifeabunike Onyedika 👋</h1>
-      <h2 className="mb-4 text-2xl text-indigo-600">Front-end Developer</h2>
+    <section className="flex flex-col items-center justify-center px-4 py-24 mt-20 text-center">
+      <h1 className="mb-4 text-4xl font-bold">Hi, I'm Ifeabunike Onyedika</h1>
+      <h2 className="mb-4 text-2xl text-gray-900">Front-End Developer</h2>
       <p className="max-w-xl mb-6 text-gray-600">
         I build modern, responsive web interfaces using React and TailwindCSS.
       </p>
       <Link
         to="/projects"
-        className="px-6 py-3 text-white transition bg-indigo-600 rounded-full hover:bg-indigo-700"
+        className="px-6 py-3 text-white transition bg-blue-600 rounded-full hover:bg-indigo-700"
       >
         View My Work
       </Link>
